45ca417c53ab12c6c9e6c2fb758062b81eea6bd8
Jens Stephan <stephje1@gmail.com>
2023-04-01 20:39:08 +0200

    Added PostgresDB.

---
+++ app/app/tests.py
@@ -0,0 +1,24 @@
+"""
+Sample Test
+"""
+
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    """Test the calc module."""
+
+    def test_add_numbers(self):
+        """Test adding numbers together."""
+        res = calc.add(5, 7)
+
+        self.assertEqual(res, 11)
+
+    
+    def test_substract_numbers(self):
+        """Test substracting numbers."""
+        res = calc.substract(10, 15)
+
+        self.assertEqual(res, 5)
