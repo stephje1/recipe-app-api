2c2deb1df0eb34ca0288e70af2d4ad0ecb67927b
Jens Stephan <stephje1@gmail.com>
2023-04-02 15:37:19 +0200

    Configured docker compose and checks to wait for db

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -2,9 +2,7 @@
 Test custom Django management commands.
 """
 from unittest.mock import patch
-
 from psycopg2 import OperationalError as Psycopg2Error
-
 from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
@@ -17,19 +15,14 @@ class CommandTests(SimpleTestCase):
     def test_wait_for_db_ready(self, patched_checked):
         """Test waiting for database if database ready."""
         patched_checked.return_value = True
-
         call_command('wait_for_db')
-
         patched_checked.assert_called_once_with(databases=['default'])
-
 
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_checked):
         """Test waiting for database when getting OperationalError."""
         patched_checked.side_effect = [Psycopg2Error] * 2 + \
             [OperationalError] * 3 + [True]
-        
         call_command('wait_for_db')
-        
         self.assertEqual(patched_checked.call_count, 6)
         patched_checked.assert_called_with(databases=['default'])
