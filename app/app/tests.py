a3bca2c4dc23b5f4a83db7fe215852835dc87964
Jens Stephan <stephje1@gmail.com>
2023-04-02 15:29:41 +0200

    Configured docker compose and checks to wait for db

--- app/app/tests.py
+++ app/app/tests.py
@@ -7,12 +7,10 @@ from app import calc
 
 class CalcTests(SimpleTestCase):
     """Test the calc module."""
-    
     def test_add_numbers(self):
         """Test adding numbers together."""
         res = calc.add(5, 7)
         self.assertEqual(res, 12)
-    
     def test_substract_numbers(self):
         """Test substracting numbers."""
         res = calc.substract(15, 10)
